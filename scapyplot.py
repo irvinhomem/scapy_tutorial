3b13b8b31669cef612c64ed5dcdbf9ce05bb45ac
irvin <irvinhomem@gmail.com>
2016-01-05 23:30:25 +0100

    Added scapy with matplotlib plotting graph example

---
+++ scapyplot.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+matplotlib.get_backend()
+#plt.ion()
+
+ans,unans=sr(IP(dst="www.bbc.co.uk")/TCP(sport=[RandShort()]*200), timeout=1)
+
+plt.show(ans.plot(lambda x: x[1].dport))
+
