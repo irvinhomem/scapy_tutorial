d77b7095165f79f7cf63e539ab04087a39a9225c
Irvin H <irvinhomem@gmail.com>
2016-01-13 00:28:37 +0100

    Extended types of packets sent and received

--- scapymultipktplot.py
+++ scapymultipktplot.py
@@ -6,8 +6,17 @@ from numpy.matlib import rand
 #a = rand(100)
 #b = rand(100)
 
+# ICMP packets
 #myPkt=sr1(IP(dst="www.slashdot.org")/ICMP()/"XXXXXXXXXXX")
-ans,unans=sr(IP(dst="www.slashdot.org")/TCP(sport=[RandShort()]*10), timeout=1)
+
+# HTTP ??
+#ans,unans=sr(IP(dst="www.slashdot.org")/TCP(sport=[RandShort()]*10), timeout=1)
+#ans,unans=sr(IP(dst="dsv.su.se")/TCP()/"GET /index.html HTTP/1.0 \n\n")     ## Not working
+
+# DNS
+#ans,unans=sr(IP(dst="8.8.8.8")/UDP()/DNS(rd=1,qd=DNSQR(qname="www.slashdot.org")))
+#ans,unans=sr(IP(dst="8.8.8.8")/UDP()/DNS(rd=1,qd=DNSQR(qname="www.facebook.com")))
+ans,unans=sr(IP(dst="8.8.8.8")/UDP()/DNS(rd=1,qd=DNSQR(qname="www.google.com")))
 
 #plt.show(ans.plot(lambda x: x[1].id))
 #plt.show(ans.scatter(lambda x: x[1].id))
@@ -26,6 +35,7 @@ print("pktidSeq datatype: ", type(pktidSeq))
 print("pktlenSeq datatype: ", type(pktlenSeq))
 
 #Create Sequence of id's and sequence of lengths
+#ans object contains a tuple of 2 lists the sent packets (snd) and the received packet in response (rcv)
 for snd,rcv in ans:
     pktidSeq.append(rcv.id)
     pktlenSeq.append(rcv.len)
